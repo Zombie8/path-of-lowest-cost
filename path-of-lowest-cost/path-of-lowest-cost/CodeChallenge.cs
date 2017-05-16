fa513a81c064d0dd17b2aad7686a57b9abbd15bb
Zack Snyder <zack.m.snyder@gmail.com>
2017-05-15 22:06:49 -0400

    initial projects commit.

---
+++ path-of-lowest-cost/path-of-lowest-cost/CodeChallenge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace path_of_lowest_cost
+{
+    public class CodeChallenge
+    {
+        public CodeChallenge()
+        {
+        }
+    }
+}
---
+++ path-of-lowest-cost/path-of-lowest-cost/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace path_of_lowest_cost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var challenge = new CodeChallenge();
+
+
+        }
+    }
+}
