db5d56f732447fafa24ed7d93fc11f9c65a94464
Zack Snyder <zack.m.snyder@gmail.com>
2017-05-21 16:26:11 -0400

    got single and double matrix logic completed, not moving onto selecting correct column value.

--- path-of-lowest-cost/path-of-lowest-cost/CodeChallenge2.cs
+++ path-of-lowest-cost/path-of-lowest-cost/CodeChallenge2.cs
@@ -1,40 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace path_of_lowest_cost
 {
     public class CodeChallenge2
     {
         private readonly int[,] _grid;
-        private string _isSolved;
-        private int _solutionTotal;
-        private List<int> _selectedColumns;
+        private List<Attempt> _attempts;
 
         public CodeChallenge2(int[,] grid)
         {
             _grid = grid;
-            _isSolved = "Yes";
-            _solutionTotal = 0;
-            _selectedColumns = new List<int>();
+            _attempts = new List<Attempt>();
         }
 
-        public Tuple<string, int, List<int>> SolveChallenge()
+        public Attempt SolveChallenge()
         {
-            var row = 0;
-
-            for(int i = 0; i < _grid.GetLength(1); i++)
+            // iterate through each row in grid
+            for (int i = 0; i < _grid.GetLength(0); i++)
             {
-                _solutionTotal += _grid[row, i];
-                _selectedColumns.Add(row + 1);
+                attemptChallenge(i);
+            }
 
-                if (_solutionTotal > 50)
+            // determine which iteration was the least cost
+            var leastCostAttempt = _attempts.OrderBy(x => x.solutionTotal).First();
+
+            // return least cost row
+            return leastCostAttempt;
+        }
+
+        private void attemptChallenge(int row)
+        {
+            var newAttempt = new Attempt();
+
+            for (int i = 0; i < _grid.GetLength(1); i++)
+            {
+                newAttempt.solutionTotal += _grid[row, i];
+                newAttempt.selectedMatrixPoints.Add(row + 1);
+
+                if (newAttempt.solutionTotal > 50)
                 {
-                    _isSolved = "No";
+                    newAttempt.isSolved = "No";
                     break;
                 }
             }
 
-            return new Tuple<string, int, List<int>>(_isSolved, _solutionTotal, _selectedColumns);
+            _attempts.Add(newAttempt);
+        }
+    }
+
+    public class Attempt
+    {
+        public string isSolved { get; set; }
+        public int solutionTotal { get; set; }
+        public List<int> selectedMatrixPoints { get; set; }
+
+        public Attempt()
+        {
+            isSolved = "Yes";
+            selectedMatrixPoints = new List<int>();
         }
     }
 }
--- path-of-lowest-cost/path-of-lowest-cost/Program.cs
+++ path-of-lowest-cost/path-of-lowest-cost/Program.cs
@@ -6,17 +6,22 @@ namespace path_of_lowest_cost
     {
         static void Main(string[] args)
         {
-            //var testGrid = testGrid1();       // YES, 5, [1, 1, 1, 1, 1];
-            //var testGrid = testGrid2();       // YES, 50, [1, 1, 1, 1, 1];
-            //var testGrid = testGrid3();       // NO, 51, [1, 1, 1];
-            var testGrid = testGrid4();         // YES, 0, [1, 1, 1, 1, 1];
+            //var testGrid = testGrid1();       // YES, 5, [1, 1, 1, 1, 1]
+            //var testGrid = testGrid2();       // YES, 50, [1, 1, 1, 1, 1]
+            //var testGrid = testGrid3();       // NO, 51, [1, 1, 1]
+            //var testGrid = testGrid4();       // YES, 0, [1, 1, 1, 1, 1]
+            //var testGrid = testGrid5();       // YES, 5, [1, 1, 1, 1, 1]
+            //var testGrid = testGrid6();       // YES, 5, [2, 2, 2, 2, 2]
+            //var testGrid = testGrid7();       // YES, 5, [1, 2, 1, 2, 1]
+            var testGrid = testGrid8();         // YES, 5, [2, 1, 2, 1, 2]
+            //var testGrid = testGrid9();       // YES, 5, [3, 3, 3, 3, 3]
 
             var challenge = new CodeChallenge2(testGrid);
             var result = challenge.SolveChallenge();
 
-            Console.WriteLine(result.Item1);
-            Console.WriteLine(result.Item2.ToString());
-            foreach(var value in result.Item3)
+            Console.WriteLine(result.isSolved);
+            Console.WriteLine(result.solutionTotal.ToString());
+            foreach(var value in result.selectedMatrixPoints)
             {
                 Console.Write(value.ToString() + ", ");
             }
@@ -24,9 +29,9 @@ namespace path_of_lowest_cost
             Console.ReadLine();
         }
 
-        //==========================//
-        // SINGLE DIMENSION MATRIXS //
-        //==========================//
+        //===========================//
+        // SINGLE DIMENSION MATRICES //
+        //===========================//
         static int[,] testGrid1()
         {
             int[,] testGrid = new int[1, 5];
@@ -75,10 +80,46 @@ namespace path_of_lowest_cost
             return testGrid;
         }
 
-        //==========================//
-        // DOUBLE DIMENSION MATRIXS //
-        //==========================//
+        //===========================//
+        // DOUBLE DIMENSION MATRICES //
+        //===========================//
         static int[,] testGrid5()
+        {
+            int[,] testGrid = new int[2, 5];
+            testGrid[0, 0] = 1;
+            testGrid[0, 1] = 1;
+            testGrid[0, 2] = 1;
+            testGrid[0, 3] = 1;
+            testGrid[0, 4] = 1;
+
+            testGrid[1, 0] = 2;
+            testGrid[1, 1] = 2;
+            testGrid[1, 2] = 2;
+            testGrid[1, 3] = 2;
+            testGrid[1, 4] = 2;
+
+            return testGrid;
+        }
+
+        static int[,] testGrid6()
+        {
+            int[,] testGrid = new int[2, 5];
+            testGrid[0, 0] = 2;
+            testGrid[0, 1] = 2;
+            testGrid[0, 2] = 2;
+            testGrid[0, 3] = 2;
+            testGrid[0, 4] = 2;
+
+            testGrid[1, 0] = 1;
+            testGrid[1, 1] = 1;
+            testGrid[1, 2] = 1;
+            testGrid[1, 3] = 1;
+            testGrid[1, 4] = 1;
+
+            return testGrid;
+        }
+
+        static int[,] testGrid7()
         {
             int[,] testGrid = new int[2, 5];
             testGrid[0, 0] = 1;
@@ -92,6 +133,51 @@ namespace path_of_lowest_cost
             testGrid[1, 2] = 2;
             testGrid[1, 3] = 1;
             testGrid[1, 4] = 2;
+
+            return testGrid;
+        }
+
+        static int[,] testGrid8()
+        {
+            int[,] testGrid = new int[2, 5];
+            testGrid[0, 0] = 2;
+            testGrid[0, 1] = 1;
+            testGrid[0, 2] = 2;
+            testGrid[0, 3] = 1;
+            testGrid[0, 4] = 2;
+
+            testGrid[1, 0] = 1;
+            testGrid[1, 1] = 2;
+            testGrid[1, 2] = 1;
+            testGrid[1, 3] = 2;
+            testGrid[1, 4] = 1;
+
+            return testGrid;
+        }
+
+        //===========================//
+        // TRIPLE DIMENSION MATRICES //
+        //===========================//
+        static int[,] testGrid9()
+        {
+            int[,] testGrid = new int[3, 5];
+            testGrid[0, 0] = 3;
+            testGrid[0, 1] = 3;
+            testGrid[0, 2] = 3;
+            testGrid[0, 3] = 3;
+            testGrid[0, 4] = 3;
+
+            testGrid[1, 0] = 2;
+            testGrid[1, 1] = 2;
+            testGrid[1, 2] = 2;
+            testGrid[1, 3] = 2;
+            testGrid[1, 4] = 2;
+
+            testGrid[2, 0] = 1;
+            testGrid[2, 1] = 1;
+            testGrid[2, 2] = 1;
+            testGrid[2, 3] = 1;
+            testGrid[2, 4] = 1;
 
             return testGrid;
         }
